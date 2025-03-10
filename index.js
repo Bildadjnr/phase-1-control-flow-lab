f9111bbd116d3abd1cb67ab68969a7814509e86e
Bildad Ereggae <bildadereggae@gmail.com>
2025-03-11 00:22:47 +0300

    control flow lab completed

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+   if (distance <= 400) {
+     return "This one is on me!";
+   } else if (distance > 400 && distance <= 2000) {
+     return "That will be twenty bucks.";
+   } else if (distance > 2000 && distance < 2500) {
+     return "I will gladly take your thirty bucks.";
+   } else if (distance > 2500) {
+     return "No can do.";
+   }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+    let theCity;
+    city === "NYC" ? (theCity = "Ok, sounds good.") : (theCity = "No go.");
+    return theCity;
+}
+  
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let answer;
+  switch (tip) {
+    case "generous":
+      answer = "Thank you so much.";
+      break;
+    case "not as generous":
+      answer = "Thank you.";
+      break;
+    default:
+      answer = "Bye.";
+      break;
+  }
+  return answer;
 }
